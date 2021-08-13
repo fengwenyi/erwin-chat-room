936615962f56e2a8c5cea942779e02679a605965
冯文议 <xfsy_2015@163.com>
2021-08-13 09:53:56 +0800

    init

---
+++ src/main/java/com/fengwenyi/erwinchatroom/ErwinChatRoomApplication.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinchatroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErwinChatRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErwinChatRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fengwenyi/erwinchatroom/ErwinChatRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengwenyi.erwinchatroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErwinChatRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
