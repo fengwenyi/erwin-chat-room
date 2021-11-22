b3d4790425eb64f456fcf2165842e75280cc8992
冯文议 <xfsy_2015@163.com>
2021-11-22 18:00:18 +0800

    spring boot 2.6.0

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+version=1.1.3
 mvn clean package -DskipTests
-docker build -t fengwenyi/erwin-chat-room:1.1.2 .
-docker tag fengwenyi/erwin-chat-room:1.1.2 fengwenyi/erwin-chat-room:1.1.2
-docker push fengwenyi/erwin-chat-room:1.1.2
-git tag -a 1.1.2 -m 'v1.1.2'
-git push origin 1.1.2
+docker build -t fengwenyi/erwin-chat-room:$version .
+docker tag fengwenyi/erwin-chat-room:$version fengwenyi/erwin-chat-room:$version
+docker push fengwenyi/erwin-chat-room:$version
+git tag -a $version -m "v$version"
+git push origin $version
