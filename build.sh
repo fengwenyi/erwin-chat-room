7f9e749e3674d6328399d97e176732d4be59633e
ErwinFeng <xfsy_2015@163.com>
2022-07-17 00:21:19 +0800

    切换 jdk 版本

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 version=`awk '/<version>[^<]+<\/version>/{gsub(/<version>|<\/version>/,"",$1);print $1;exit;}' pom.xml`
 echo $version
+jdk17
 mvn clean package -DskipTests
 docker build -t fengwenyi/erwin-chat-room .
 docker tag fengwenyi/erwin-chat-room fengwenyi/erwin-chat-room
